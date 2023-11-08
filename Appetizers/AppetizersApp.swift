06bc068fde93fa88606174bc56faa3751fd3bd18
Halil YAŞ <106011017+halilyass@users.noreply.github.com>
2023-11-08 14:27:39 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Halil YAŞ on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
